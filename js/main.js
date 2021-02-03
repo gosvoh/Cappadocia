683a9eb0fd359fcd43ec95d04db71611ea31ec20
Aleksey Vokhmin <vohmina2011@yandex.ru>
2021-02-03 13:30:16 +0300

    Moved to SCSS Added carousel (slick)

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    // noinspection HtmlUnknownTarget
+    $('.carousel').slick({
+        autoplay: true,
+        arrows: true,
+        prevArrow: '<a class="prev-arrow"><img src="img/prev.svg" alt="prev"/></a>',
+        nextArrow: '<a class="next-arrow"><img src="img/next.svg" alt="prev"/></a>'
+    });
+});
